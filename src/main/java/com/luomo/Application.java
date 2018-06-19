b60580822367f274bc6ac4dcea0d45fae55e81fe
LuoMo92 <810754420@qq.com>
2018-06-19 16:17:47 +0800

    添加线程池关闭的配置

--- src/main/java/com/luomo/Application.java
+++ src/main/java/com/luomo/Application.java
@@ -57,6 +57,10 @@ public class Application {
             executor.setThreadNamePrefix("taskExecutor-");
             //线程池对拒绝任务的处理策略：这里采用了CallerRunsPolicy策略，当线程池没有处理能力的时候，该策略会直接在 execute 方法的调用线程中运行被拒绝的任务；如果执行程序已关闭，则会丢弃该任务
             executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+            //设置线程池关闭的时候等待所有任务都完成再继续销毁其他的Bean
+            executor.setWaitForTasksToCompleteOnShutdown(true);
+            //设置线程池中任务的等待时间，如果超过这个时候还没有销毁就强制销毁，以确保应用最后能够被关闭，而不是阻塞住
+            executor.setAwaitTerminationSeconds(60);
             return executor;
         }
     }
